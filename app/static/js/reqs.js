098ba7e15bbff55d1bad85e1d10342b201cf2732
Guilherme Salazar <gmesalazar@gmail.com>
2016-09-14 14:35:28 -0300

    delete commented out code

--- app/static/js/reqs.js
+++ app/static/js/reqs.js
@@ -2,15 +2,10 @@ $(document).ready(function() {
 
 	$.getJSON("/api/request/all", function(data) {
 		var viewModel = {
-//		requests : ko.observableArray([
-//			{id: 1, title: 'alo', desc: 'descricao', client:'cliente',
-//			priority:'prioridade', deadline:'deadline', url:'url', prodarea:'prod'}
-//		])
-		requests : ko.observableArray(data)
-	};
+			requests : ko.observableArray(data)
+		};
 
-	ko.applyBindings(viewModel);
-	
+		ko.applyBindings(viewModel);
 	});
 
 });
