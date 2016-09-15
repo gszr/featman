e5ffe34c02a084557a3f96969a0e8053816146fa
Guilherme Salazar <gmesalazar@gmail.com>
2016-09-15 19:26:58 -0300

    first step towards table sorting, thanks to github.com/github.com/pstricker/koTableSort

--- app/static/js/reqs.js
+++ app/static/js/reqs.js
@@ -1,14 +1,22 @@
 var viewModel = {
-	requests : ko.observableArray([])
+	columns  : ko.observableArray([
+		{col: "id", val: "#", type: "string", state: ko.observable("")},
+		{col: "title", val: "Title", type: "string", state: ko.observable("")},
+		{col: "descr", val: "Description", type: "string", state: ko.observable("")},
+		{col: "client", val: "Client", type: "string", state: ko.observable("")},
+		{col: "priority", val: "Priority", type: "string", state: ko.observable("")},
+		{col: "deadline", val: "Target Date", type: "string", state: ko.observable("")},
+		{col: "url", val: "Ticket URL", type: "string", state: ko.observable("")},
+		{col: "prodarea", val: "Product Area", type: "string", state: ko.observable("")},
+	]),
+	features : ko.observableArray([])
 };
 
 ko.applyBindings(viewModel);
 
 
 $(document).ready(function() {
-
 	$.getJSON("/api/feature", function(data) {
-		viewModel.requests(data);
+		viewModel.features(data);
 	});
-
 });
