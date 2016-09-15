8a6e6e6c86ba8eea286dee22c93095ef2a6b1a69
Guilherme Salazar <gmesalazar@gmail.com>
2016-09-15 18:18:35 -0300

    fix responsiveness on "all requests" page

--- app/static/js/reqs.js
+++ app/static/js/reqs.js
@@ -1,11 +1,14 @@
+var viewModel = {
+	requests : ko.observableArray([])
+};
+
+ko.applyBindings(viewModel);
+
+
 $(document).ready(function() {
 
 	$.getJSON("/api/feature", function(data) {
-		var viewModel = {
-			requests : ko.observableArray(data)
-		};
-
-		ko.applyBindings(viewModel);
+		viewModel.requests(data);
 	});
 
 });
