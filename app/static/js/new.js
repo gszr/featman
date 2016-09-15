66c78fa0ad221eb1cc8320eb48118b13eaab5747
Guilherme Salazar <gmesalazar@gmail.com>
2016-09-15 15:03:17 -0300

    "new feature" page: fix client, product, and priority fields

--- app/static/js/new.js
+++ app/static/js/new.js
@@ -1,12 +1,15 @@
 $(document).ready(function() {
 	function ViewModel() {
-		this.title    = ko.observable();
-		this.descr    = ko.observable();
-		this.client   = ko.observable();
-		this.priority = ko.observable();
-		this.deadline = ko.observable();
-		this.url      = ko.observable();
-		this.prodarea = ko.observable();
+		this.title     = ko.observable();
+		this.descr     = ko.observable();
+		this.client    = ko.observable();
+		this.clients   = ko.observableArray(["ClientA", "ClientB", "ClientC"]);
+		this.priority  = ko.observable();
+		this.deadline  = ko.observable();
+		this.url       = ko.observable();
+		this.prodarea  = ko.observable();
+		this.prodareas = ko.observableArray(["Policies", "Billing", "Claims",
+			"Reports"]);
 
 		this.submitReq = function() {
 			$.ajax({
