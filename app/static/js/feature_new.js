edec79cd8e8d22b225e312d5d748793c77e17f6f
Guilherme Salazar <gmesalazar@gmail.com>
2016-09-16 15:02:24 -0300

    integrate requests & clients interface

--- app/static/js/feature_new.js
+++ app/static/js/feature_new.js
@@ -1,32 +1,40 @@
-$(document).ready(function() {
-	function ViewModel() {
-		this.title     = ko.observable();
-		this.descr     = ko.observable();
-		this.client    = ko.observable();
-		this.clients   = ko.observableArray(["ClientA", "ClientB", "ClientC"]);
-		this.priority  = ko.observable();
-		this.deadline  = ko.observable();
-		this.url       = ko.observable();
-		this.prodarea  = ko.observable();
-		this.prodareas = ko.observableArray(["Policies", "Billing", "Claims",
-			"Reports"]);
+var viewModel = {
+	title     : ko.observable(),
+	descr     : ko.observable(),
+	client    : ko.observable(),
+	clients   : ko.observableArray([
+		{id: 1, name: "ClientA"},
+		{id: 2, name: "ClientB"},
+		{id: 3, name: "ClientC"}]),
+	priority  : ko.observable(),
+	deadline  : ko.observable(),
+	url       : ko.observable(),
+	prodarea  : ko.observable(),
+	prodareas : ko.observableArray(["Policies", "Billing", "Claims",
+		"Reports"])
 
-		this.submitReq = function() {
-			$.ajax({
-				type : "POST",
-				url  : "/api/feature",
-				contentType: "application/json; charset=utf-8",
-				data : ko.toJSON(this),
-				dataType : "json",
-				success: function(returnedData) {
-					window.location.href = "/feature/all"
-				}
-				//TODO handle failure as well
-			})
+};
+
+viewModel.submitReq = function() {
+	$.ajax({
+		type : "POST",
+		url  : "/api/feature",
+		contentType: "application/json; charset=utf-8",
+		data : ko.toJSON(viewModel),
+		dataType : "json",
+		success: function(returnedData) {
+			window.location.href = "/feature/all"
 		}
-	};
+		//TODO handle failure as well
+	})
+}
 
-	ko.applyBindings(new ViewModel());
+ko.applyBindings(viewModel);
+
+$(document).ready(function() {
+	$.getJSON("/api/client", function(data) {
+		viewModel.clients(data);
+	});
 
 	$('.datepicker').datepicker({
 		maxViewMode: 3,
