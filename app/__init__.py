012e998a9e75b9ea82045d66c9ba9d04994b8f85
Guilherme Salazar <gmesalazar@gmail.com>
2016-09-19 19:57:02 -0300

    create wsgi config file & fix db path

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+import os
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///featreq.db'
+db_file = os.path.join(os.path.dirname(__file__), 'featreq.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///{}'.format(db_file)
 db  = SQLAlchemy(app)
 
 import models
---
+++ app/application.wsgi
@@ -0,0 +1,4 @@
+import sys
+sys.path.append('/var/www/FlaskApps/featreq')
+
+from run import app as application
